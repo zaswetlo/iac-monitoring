fb1aafe6adcaa46d816bb9139d78321d548adf8d
Mariia <80515493+zaswetlo@users.noreply.github.com>
2026-09-11 13:13:51 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "prometheus_port" {
+  description = "Port exposed by Prometheus"
+  type        = number
+  default     = 9090
+}
+
+variable "grafana_port" {
+  description = "Port exposed by Grafana"
+  type        = number
+  default     = 3000
+}
+
+variable "prometheus_image" {
+  description = "Prometheus Docker image"
+  type        = string
+  default     = "prom/prometheus:latest"
+}
+
+variable "grafana_image" {
+  description = "Grafana Docker image"
+  type        = string
+  default     = "grafana/grafana:latest"
+}
